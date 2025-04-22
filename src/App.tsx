3bd47a9e24fc3f2ea02927d8045dfd7f2507bb97
Rayyan Akhter <rayyanakhter2003@gmail.com>
2025-04-23 03:25:33 +0530

    Fix : first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from 'react-redux';
-import { store } from './store';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import { store } from './store';
 
 const queryClient = new QueryClient();
 
@@ -15,8 +13,6 @@ const App = () => (
   <Provider store={store}>
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        <Toaster />
-        <Sonner />
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
